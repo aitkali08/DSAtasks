31843acc5195242d8155c4e6b628adb49bc6f811
aitkali08 <241625@astanait.edu.kz>
2025-03-30 20:22:00 +0500

    Update Problem10.java

--- src/Assignment1/Problem10.java
+++ src/Assignment1/Problem10.java
@@ -1,7 +1,15 @@
 package Assignment1;
 
 import java.util.Scanner;
-
+/**
+ * This method calculates the Greatest Common Divisor (GCD) of two numbers using recursion.
+ * 
+ * The function implements the **Euclidean Algorithm**, which states:
+ * GCD(a, b) = GCD(b, a % b) until b becomes 0, at which point a is the GCD.
+ * 
+ * Time Complexity: O(log(min(a, b))), since the Euclidean algorithm runs in logarithmic time.
+ * Space Complexity: O(log(min(a, b))), due to the recursive call stack.
+ */
 public class Problem10 {
     public static int findGCD(int a, int b) {
         if (b == 0) {
