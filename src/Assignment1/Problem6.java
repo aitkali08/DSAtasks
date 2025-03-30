7f5b56457ed95e1d3deec9a199df547bb65ae5b0
aitkali08 <241625@astanait.edu.kz>
2025-03-30 20:45:42 +0500

    Update Problem6.java

--- src/Assignment1/Problem6.java
+++ src/Assignment1/Problem6.java
@@ -1,6 +1,20 @@
 package Assignment1;
 
 import java.util.Scanner;
+/**
+ * This method calculates the power of a number using recursion.
+ * 
+ * The function computes a^n (a raised to the power of n) recursively.
+ * 
+ * Base Case:
+ * - If n == 0, return 1 (as any number raised to the power of 0 is 1).
+ * 
+ * Recursive Case:
+ * - Multiply 'a' by the result of power(a, n - 1).
+ * 
+ * Time Complexity: O(n), since the function calls itself n times.
+ * Space Complexity: O(n), due to recursive function calls stored in the call stack.
+*/
 
 public class Problem6 {
     public static long power(int a, int n) {
