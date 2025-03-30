bae192421ce142cdcb5e1753e317b760f639e7eb
aitkali08 <241625@astanait.edu.kz>
2025-03-30 20:47:41 +0500

    Update Problem5.java

--- src/Assignment1/Problem5.java
+++ src/Assignment1/Problem5.java
@@ -1,7 +1,24 @@
 package Assignment1;
 
 import java.util.Scanner;
-
+/**
+ * This method calculates the nth Fibonacci number using recursion.
+ * 
+ * The Fibonacci sequence is defined as:
+ * - F(0) = 0
+ * - F(1) = 1
+ * - F(n) = F(n-1) + F(n-2) for n ≥ 2
+ * 
+ * Base Cases:
+ * - If n == 0, return 0.
+ * - If n == 1, return 1.
+ * 
+ * Recursive Case:
+ * - Compute F(n) as the sum of F(n-1) and F(n-2).
+ * 
+ * Time Complexity: O(2^n), due to repeated calculations in the recursive tree.
+ * Space Complexity: O(n), due to recursive function calls stored in the call stack
+    */
 public class Problem5 {
     public static int fibonacci(int n) {
         if (n == 0) {
